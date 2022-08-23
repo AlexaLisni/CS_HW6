ca96a324d2eaa4c42b56c57171c56c936e8ddc3e
Alexandr <sashylay0@gmail.com>
2022-08-23 14:17:02 +0300

    finish task43

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+Console.WriteLine("Решаем уравнения y = k1 * x + b1 и y = k2 * x + b2 ");
+Console.WriteLine("задайте значения:");
+
+Console.Write("k1-> ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2-> ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1-> ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2-> ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+void IntPoint(double k1, double k2, double b1, double b2 )
+{
+double x =(b2 - b1)/(k1 - k2);
+double y1= (k1 * x) + b1;
+double y2 = (k2* x) + b2;
+Console.WriteLine($"({y1}; {y2})");
+}
+IntPoint(k1, k2, b1, b2);
